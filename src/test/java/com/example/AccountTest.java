df408329eacf9ac2ce8521db28c1e510b3ec9bef
Chadrac-WAKOMYA <chadracxx@gmail.com>
2026-06-08 20:29:49 +0200

    fit test amorced

--- src/test/java/com/example/AccountTest.java
+++ src/test/java/com/example/AccountTest.java
@@ -8,8 +8,50 @@ public class AccountTest {
 
     @Test
     public void accountCannotHaveNegativeOverdraftLimit() {
-        Account account = new Account(-20);
-        
+        Account account = new Account(-20);        
         Assert.assertEquals(0d, account.getOverdraftLimit(), epsilon);
+    }
+
+    @Test
+    public void accountCanHavePositiveOverdraftLimit() {
+        Account account = new Account(20);        
+        Assert.assertEquals(20d, account.getOverdraftLimit(), epsilon);
+    }
+
+    @Test
+    public void accountCantAcceptNegativeDeposit() {
+        Account account = new Account(100);        
+        boolean result = account.deposit(-10);
+        Assert.assertFalse(result);
+    }
+
+    @Test
+    public void accountCantAcceptNegativeWithdrawal() {
+        Account account = new Account(100);        
+        boolean result = account.withdraw(-10);
+        Assert.assertFalse(result);
+    }
+
+    @Test
+    public void accountCantWithdrawMoreThanOverdraftLimit() {
+        Account account = new Account(100);        
+        boolean result = account.withdraw(150);
+        Assert.assertFalse(result);
+    }
+
+    @Test
+    public void withdrawReturnsACorrectBalance() {
+        Account account = new Account(100);        
+        account.deposit(50);
+        boolean result = account.withdraw(120);
+        Assert.assertFalse(result);
+        Assert.assertEquals(50, account.getBalance(), epsilon);
+    }
+
+    @Test
+    public void depositReturnsACorrectBalance() {
+        Account account = new Account(100);        
+        account.deposit(50);
+        Assert.assertEquals(50, account.getBalance(), epsilon);
     }
 }
